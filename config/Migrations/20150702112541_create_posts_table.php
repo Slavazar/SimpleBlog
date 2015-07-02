8a1b2400143221e5e91b279d2a6ce9f60ad86c13
slava <slava@localhost-ubuntu>
2015-07-02 14:35:15 +0300

    Migration

---
+++ config/Migrations/20150702112541_create_posts_table.php
@@ -0,0 +1,57 @@
+<?php
+use Phinx\Migration\AbstractMigration;
+
+class CreatePostsTable extends AbstractMigration
+{
+    public function up()
+    {
+        $table = $this->table('posts');
+        $table
+            ->addColumn('name', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => false,
+            ])
+            ->addColumn('slug', 'string', [
+                'default' => null,
+                'limit' => 100,
+                'null' => true,
+            ])
+            ->addColumn('content', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('excerpt', 'text', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('image', 'string', [
+                'default' => null,
+                'limit' => 255,
+                'null' => true,
+            ])
+            ->addColumn('published', 'boolean', [
+                'default' => 0,
+                'limit' => null,
+                'null' => false,
+            ])
+            ->addColumn('created', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->addColumn('modified', 'datetime', [
+                'default' => null,
+                'limit' => null,
+                'null' => true,
+            ])
+            ->create();
+    }
+
+    public function down()
+    {
+        $this->dropTable('posts');
+    }
+}
